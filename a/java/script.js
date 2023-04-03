6ddacf152bcf664ea89c97fbb4922bcd9bf68e15
Gustavo Sousa <125740562+Skybr23@users.noreply.github.com>
2023-04-03 18:47:32 -0300

    Add files via upload

--- a/java/script.js
+++ a/java/script.js
@@ -3,19 +3,19 @@ var position
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     btn.addEventListener("touchstart", function() {
-        position ? (position = 0) : (position = 1500);
+        position ? (position = 0) : (position = 15000);
         btn.style.transform = `translate(${position}px,0px)`;
         btn.style.transition = "all 0.1s ease";
       });
       
       btn.addEventListener("touchend", function() {
-        position ? (position = 0) : (position = 1500);
+        position ? (position = 0) : (position = 15000);
         btn.style.transform = `translate(${position}px,0px)`;
         btn.style.transition = "all 0.1s ease";
       });
   } else {
     btn.addEventListener("mouseover", function() {
-        position ? (position = 0) : (position = 1500);
+        position ? (position = 0) : (position = 15000);
         btn.style.transform = `translate(${position}px,0px)`;
         btn.style.transition = "all 0.1s ease";
         });
